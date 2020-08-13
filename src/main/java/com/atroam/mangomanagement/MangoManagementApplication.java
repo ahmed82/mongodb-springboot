6658f13987c173165a7d7b936fbb84382f5c2da1
Ahmed Al-Salih <Ahmed.Al-Salih@retailbusinessservices.com>
2020-08-13 16:37:46 -0400

    start Restful project performe CRUD operation to MOngoDB

---
+++ src/main/java/com/atroam/mangomanagement/MangoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.atroam.mangomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atroam/mangomanagement/MangoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atroam.mangomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
